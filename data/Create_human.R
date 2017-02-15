84dbc62f95fd0610ffadcefe79c18290b24a3ab0
Mannerheim <Tuomassoila@Tuomas-MacBook-Air.local>
2017-02-15 14:08:13 +0200

    chapter4 and data wrangling started

---
+++ data/Create_human.R
@@ -0,0 +1,2 @@
+gii <- read.csv("http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/gender_inequality.csv", stringsAsFactors = F, na.strings = "..")
+hd <- read.csv(http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/human_development.csv", stringsAsFactors = F)
